abdb134acae78fcd6c8c1b561ab57042f9062d93
Robley <robleyadrian@gmail.com>
2019-12-29 20:20:59 +0300

    modify allowed hosts

--- django_ec2_project/django_ec2_project/settings.py
+++ django_ec2_project/django_ec2_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o&51*r9z_fdf*3$eh8zkgku=3$o(xyf9e81d5n^srw&@8+ok=s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
